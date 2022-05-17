5c7b89335515b5873fea7607db4e54817958ad22
KotaFujisawa <tengzehongduo@gmail.com>
2022-05-17 15:06:23 +0900

    ADD　おれがfizzbuzzconrollerをついかした

---
+++ src/main/java/com/example/demo/FizzBuzzController.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FizzBuzzController {
+
+	@RequestMapping("/")
+	public String top() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/apply", method = RequestMethod.POST)
+	public ModelAndView top(
+			@RequestParam("num") int num,
+			ModelAndView mv
+			) {
+//		if (num % 3 == 0&&num % 5 == 0) {
+//			mv.addObject("result", "Fizz!! Buzz!! ");
+//		}else if (num % 3 == 0) {
+//			mv.addObject("result", "Fizz! ");
+//		} else if (num % 5 == 0) {
+//			mv.addObject("result", "Buzz! ");
+//		} else {
+//			mv.addObject("result", "ん？？");
+//		}
+		
+		String r ="";
+		if(num!=0) {
+		if (num % 3 == 0) {
+			r+="Fizz! ";
+		}
+		if (num % 5 == 0) {
+			r+="Buzz! ";
+		}
+		if (num % 7 == 0) {
+			r+="Ass! ";
+		}
+		
+		mv.addObject("result", r);
+		}
+		
+		
+		mv.setViewName("index");
+		return mv;
+	}
+
+}
